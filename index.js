ef9fc990023e181bca0f37dde07413c511d9e316
yatinpachori <67282395+yatinpachori@users.noreply.github.com>
2025-02-13 21:56:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// function myfunc() {
+//     var ele = document.getElementById('myDIV');
+//     ele.textContent = "Thankk you"
+// }
+
+var customalert = document.getElementById('showalert')
+
+function myfunc2() {
+    customalert.style.display = 'block';
+}
+function hidealert() {
+    customalert.style.display = 'none';
+}
+
+var customalert2 = document.getElementById('showalert2')
+
+function myfunc() {
+    customalert2.style.display = 'block';
+}
+function hidealert2() {
+    customalert2.style.display = 'none';
+}
